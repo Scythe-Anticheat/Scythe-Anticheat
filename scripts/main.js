8e7d3d79dd2b9848f0cdea783d1b5a05ff1d67ca
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2021-09-30 18:24:06 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
 import * as GameTest from "mojang-gametest";
 import { World, Commands } from "mojang-minecraft";
+
+console.warn("Im not a dumbass and this actually worked :sunglasses:")
+
+World.events.beforeChat.subscribe(msg => {
+    let message = msg.message;
+    
+    if (message == "Ping") console.warn("Pong!")
+});
