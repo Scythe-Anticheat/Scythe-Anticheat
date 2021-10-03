cf0c758ed79b23df8caa399bb82c8948b5865db0
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2021-10-02 21:22:25 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,7 @@ World.events.beforeChat.subscribe(msg => {
 });
 
 World.events.tick.subscribe(() => {
+    // run as each player
     for (let player of World.getPlayers()) {
         // Namespoof/A = username length check.
         if(player.name.length > 16)
@@ -29,5 +30,11 @@ World.events.tick.subscribe(() => {
         if (isNaN(player.location.x) || player.location.x > 30000000 || 
             isNaN(player.location.y) || player.location.y > 30000000 || 
             isNaN(player.location.z) || player.location.z > 30000000) console.warn("CRASH!!! NO!!!!!");
-    }
+
+        // fixed banning system, yay!
+        // messy af but if it works then it works
+        Commands.run(`execute ${player.name} ~~~ execute @s[scores={isBanned=3}] ~~~ kick ${player.name} "§cYOU ARE BANNED!\n\n§bReason: Sending Crash Packets\nebBanned By: Scythe AntiCheat."`, World.getDimension("overworld"));
+        Commands.run(`execute ${player.name} ~~~ execute @s[scores={isBanned=2}] ~~~ kick ${player.name} "§cYOU ARE BANNED!\n\n§bReason: Hacking Or Abuse\nebBanned By: Scythe AntiCheat."`, World.getDimension("overworld"));
+        Commands.run(`execute ${player.name} ~~~ execute @s[scores={isBanned=3}] ~~~ kick ${player.name} "§cYOU ARE BANNED!\n\n§bReason: Hacking Or Abuse\nebBanned By: An Operator."`, World.getDimension("overworld"));
+    };
 });
