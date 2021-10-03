4acdc1e9e90369d02f0623a7c3138713c1656773
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2021-10-02 19:47:35 -0600

    we can now detect for namespoof :sunglasses:

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,27 @@
 import * as GameTest from "mojang-gametest";
 import { World, Commands } from "mojang-minecraft";
 
-console.warn("Im not a dumbass and this actually worked :sunglasses:");
+var debug = true
+
+if (debug) console.warn("Im not a dumbass and this actually worked :sunglasses:");
 
 World.events.beforeChat.subscribe(msg => {
     let message = msg.message;
 
-    if (message == "Ping") console.warn("Pong!");
+    if (debug && message == "Ping") console.warn("Pong!");
 
     if (message.includes("Horion - the best minecraft bedrock utility mod - horion.download"))  msg.cancel = true;
 });
 
-World.events.tick.subscribe(tick => {
-    // bedrock filler
-    // since we are using execute @a it will also affect players in the end.
-    Commands.run(`execute @a ~~~ fill ~-10 0 ~-10 ~+10 0 ~+10 bedrock`, World.getDimension("overworld"));
-    Commands.run(`execute @a ~~~ fill ~-10 0 ~-10 ~+10 0 ~+10 bedrock`, World.getDimension("nether"));
-})
+World.events.tick.subscribe(() => {
+    for (let player of World.getPlayers()) {
+        // Namespoof/A = username length check.
+        if(player.name.length > 15) 
+            Commands.run(`execute ${player.name} ~~~ say i am a noob who uses namespoof`, World.getDimension("overworld"));
+
+        // Namespoof/B = regex check
+        let regex = /[^A-Za-z0-9_ ]/
+        if(!regex.test(plater.name))
+        Commands.run(`execute ${player.name} ~~~ say i am a noob who uses namespoof`, World.getDimension("overworld"));
+    }
+});
