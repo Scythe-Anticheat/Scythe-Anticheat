272d856f60d8eacbed47cca185a6b83cac2d28a0
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2021-12-26 23:48:34 -0600

    crasher isNaN() checks arent needed

--- scripts/main.js
+++ scripts/main.js
@@ -78,9 +78,8 @@ World.events.tick.subscribe(() => {
         } catch(error) {}
 
         // Crasher/A = invalid pos check
-        if (config.modules.crasherA.enabled && isNaN(player.location.x) || Math.abs(player.location.x) > 30000000 ||
-            isNaN(player.location.y) || Math.abs(player.location.y) > 30000000 ||
-            isNaN(player.location.z) || Math.abs(player.location.z) > 30000000) flag(player, "Crasher", "A", "Exploit", false, false, true, false);
+        if (config.modules.crasherA.enabled && Math.abs(player.location.x) > 30000000 ||
+            Math.abs(player.location.y) > 30000000 || Math.abs(player.location.z) > 30000000) flag(player, "Crasher", "A", "Exploit", false, false, true, false);
 
         // Namespoof/A = username length check.
         try {
