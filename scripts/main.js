e521ebb6ff45057492f7aee992a71a8912f45ce2
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2022-01-02 21:49:51 -0600

    slight performance improvement with gametest

--- scripts/main.js
+++ scripts/main.js
@@ -101,26 +101,34 @@ World.events.tick.subscribe(() => {
         // bedrock validation
         if (config.modules.bedrockValidate.enabled && config.modules.bedrockValidate.overworld) {
             try {
-                Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-20 -64 ~-20 ~20 -64 ~20 bedrock`, World.getDimension("overworld"));
-            } catch (error) {}
+                // only run the rest of the commands if the player is in the overworld
+                Commands.run(`testfor @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}]`, World.getDimension("overworld"));
+                try {
+                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-20 -64 ~-20 ~20 -64 ~20 bedrock`, World.getDimension("overworld"));
+                } catch (error) {}
 
-            try {
-                Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-4 -59 ~-4 ~4 319 ~4 air 0 replace bedrock`, World.getDimension("overworld"));
+                try {
+                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-4 -59 ~-4 ~4 319 ~4 air 0 replace bedrock`, World.getDimension("overworld"));
+                } catch (error) {}
             } catch (error) {}
         }
 
         if (config.modules.bedrockValidate.enabled && config.modules.bedrockValidate.nether) {
+            // only run the rest of the commands if the player is in the nether
+            Commands.run(`testfor @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}]`, World.getDimension("nether"));
             try {
-                Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-10 0 ~-10 ~10 0 ~10 bedrock`, World.getDimension("nether"));
-            } catch (error) {}
+                try {
+                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-10 0 ~-10 ~10 0 ~10 bedrock`, World.getDimension("nether"));
+                } catch (error) {}
 
-            try {
-                Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-10 127 ~-10 ~10 127 ~10 bedrock`, World.getDimension("nether"));
-            } catch (error) {}
+                try {
+                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-10 127 ~-10 ~10 127 ~10 bedrock`, World.getDimension("nether"));
+                } catch (error) {}
 
-            try {
-                Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-5 5 ~-5 ~5 120 ~5 air 0 replace bedrock`, World.getDimension("nether"));
-            } catch (error) {}
+                try {
+                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-5 5 ~-5 ~5 120 ~5 air 0 replace bedrock`, World.getDimension("nether"));
+                } catch (error) {}
+            } catch(error) {}
         }
 
         // if (debug) console.warn(`${new Date()} | ${player.name}'s vertical velocity: ${Math.abs(player.velocity.y).toFixed(4)}`);
