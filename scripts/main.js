c7e0928a06cd4b335e09c07c05197f5574cf37c1
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2022-01-27 17:56:11 -0600

    basic beta support
    haven't tested this out yet :/

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ import { banplayer } from "./data/globalban.js";
 import config from "./data/config.js";
 
 const World = Minecraft.world;
-const Commands = Minecraft.Commands;
+// const Commands = Minecraft.Commands;
 
 let loaded = false;
 
@@ -27,41 +27,29 @@ World.events.beforeChat.subscribe(msg => {
 
     // Spammer/A = checks if someone sends a message while moving and on ground
     if (config.modules.spammerA.enabled && playerTags.includes('moving') && playerTags.includes('ground') && !playerTags.includes('jump')) {
-        try {
-            Commands.run(`testfor @a[name="${player.nameTag}",tag=moving,tag=ground,tag=!jump]`, World.getDimension("overworld"));
-            flag(player, "Spammer", "A", "Movement", false, false, true, msg);
-        } catch (error) {}
+        flag(player, "Spammer", "A", "Movement", false, false, true, msg);
     }
 
     // Spammer/B = checks if someone sends a message while swinging their hand
     if (config.modules.spammerB.enabled && playerTags.includes('left')) {
-        try {
-            Commands.run(`testfor @a[name="${player.nameTag}",tag=left]`, World.getDimension("overworld"));
-            flag(player, "Spammer", "B", "Combat", false, false, false, msg);
-        } catch (error) {}
+        flag(player, "Spammer", "B", "Combat", false, false, false, msg);
     }
 
     // Spammer/C = checks if someone sends a message while using an item
     if (config.modules.spammerC.enabled && playerTags.includes('right')) {
-        try {
-            Commands.run(`testfor @a[name="${player.nameTag}",tag=right]`, World.getDimension("overworld"));
-            flag(player, "Spammer", "C", "Misc", false, false, false, msg);
-        } catch (error) {}
+        flag(player, "Spammer", "C", "Misc", false, false, false, msg);
     }
 
     // Spammer/D = checks if someone sends a message while having a GUI open
     if (config.modules.spammerD.enabled && playerTags.includes('hasGUIopen')) {
-        try {
-            Commands.run(`testfor @a[name="${player.nameTag}",tag=hasGUIopen]`, World.getDimension("overworld"));
-            flag(player, "Spammer", "D", "Misc", false, false, false, msg);
-        } catch (error) {}
+        flag(player, "Spammer", "D", "Misc", false, false, false, msg);
     }
 
     commandHandler(player, msg);
 
     // add's user custom tags to their messages
     if (player.name && player.name !== player.nameTag && !msg.cancel) {
-        Commands.run(`tellraw @a {"rawtext":[{"text":"<${player.nameTag}> ${msg.message}"}]}`, World.getDimension("overworld"));
+        player.runCommand(`tellraw @a {"rawtext":[{"text":"<${player.nameTag}> ${msg.message}"}]}`);
         msg.cancel = true;
     }
 });
@@ -72,15 +60,15 @@ World.events.tick.subscribe(() => {
     try {
         if (!loaded) {
             const players = World.getPlayers().map(player => player.nameTag);
-            Commands.run(`testfor @a[name="${players[0]}"]`, World.getDimension("overworld"));
+            World.getDimension("overworld").runCommand(`testfor @a[name="${players[0]}"]`);
             try {
                 // (1..) gametest already enabled so set loaded to true and do nothing
-                Commands.run(`testfor @a[scores={gametestapi=1..}]`, World.getDimension("overworld"));
+                World.getDimension("overworld").runCommand(`testfor @r[scores={gametestapi=1..}]`);
                 loaded = true;
             } catch {
                 // (..0) gametest needs to be enabled (1..) then set loaded to true
-                Commands.run(`testfor @a[scores={gametestapi=..0}]`, World.getDimension("overworld"));
-                Commands.run(`execute "${players[0]}" ~~~ function checks/gametestapi`, World.getDimension("overworld"));
+                World.getDimension("overworld").runCommand(`testfor @r[scores={gametestapi=..0}]`);
+                World.getDimension("overworld").runCommand(`execute "${players[0]}" ~~~ function checks/gametestapi`);
                 loaded = true;
                 return;
             }
@@ -101,16 +89,16 @@ World.events.tick.subscribe(() => {
             try {
                 // test if they have the tag first or global ban will fail if we attempt to tag with an existing tag
                 // if they are not tagged then we do that here before we ban
-                Commands.run(`testfor @a[name="${player.nameTag}",tag=!by:Scythe Anticheat]`, World.getDimension("overworld"));
-                Commands.run(`tag "${player.nameTag}" add "by:Scythe Anticheat"`, World.getDimension("overworld"));
-                Commands.run(`tag "${player.nameTag}" add "reason:You are Scythe Anticheat global banned!"`, World.getDimension("overworld"));
+                player.runCommand(`testfor @s[tag="!by:Scythe Anticheat"]`);
+                player.runCommand(`tag @s add "by:Scythe Anticheat"`);
+                player.runCommand(`tag @s add "reason:You are Scythe Anticheat global banned!"`);
             } catch (error) {}
             banMessage(player);
         }
 
         // sexy looking ban message
         try {
-            Commands.run(`testfor @a[name="${player.nameTag}",tag=isBanned]`, World.getDimension("overworld"));
+            player.runCommand(`testfor @s[tag=isBanned]`);
             banMessage(player);
         } catch(error) {}
 
@@ -139,22 +127,22 @@ World.events.tick.subscribe(() => {
 
         // player position shit
         try {
-            Commands.run(`scoreboard players set "${player.nameTag}" xPos ${Math.floor(player.location.x)}`, World.getDimension("overworld"));
-            Commands.run(`scoreboard players set "${player.nameTag}" yPos ${Math.floor(player.location.y)}`, World.getDimension("overworld"));
-            Commands.run(`scoreboard players set "${player.nameTag}" zPos ${Math.floor(player.location.z)}`, World.getDimension("overworld"));
+            player.runCommand(`scoreboard players set @s xPos ${Math.floor(player.location.x)}`);
+            player.runCommand(`scoreboard players set @s yPos ${Math.floor(player.location.y)}`);
+            player.runCommand(`scoreboard players set @s zPos ${Math.floor(player.location.z)}`);
         } catch(e) {}
 
         // bedrock validation
         if (config.modules.bedrockValidate.enabled && config.modules.bedrockValidate.overworld) {
             try {
                 // only run the rest of the commands if the player is in the overworld
-                Commands.run(`testfor @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}]`, World.getDimension("overworld"));
+                World.getDimension("overworld").runCommand(`testfor @r[name="${player.nameTag}",rm=0,scores={bedrock=1..}]`);
                 try {
-                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-20 -64 ~-20 ~20 -64 ~20 bedrock`, World.getDimension("overworld"));
+                    player.runCommand(`fill ~-20 -64 ~-20 ~20 -64 ~20 bedrock`);
                 } catch (error) {}
 
                 try {
-                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-4 -59 ~-4 ~4 319 ~4 air 0 replace bedrock`, World.getDimension("overworld"));
+                    player.runCommand(`fill ~-4 -59 ~-4 ~4 319 ~4 air 0 replace bedrock`);
                 } catch (error) {}
             } catch (error) {}
         }
@@ -162,17 +150,17 @@ World.events.tick.subscribe(() => {
         if (config.modules.bedrockValidate.enabled && config.modules.bedrockValidate.nether) {
             try {
                 // only run the rest of the commands if the player is in the nether
-                Commands.run(`testfor @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}]`, World.getDimension("nether"));
+                World.getDimension("nether").runCommand(`testfor @r[name="${player.nameTag}",rm=0,scores={bedrock=1..}]`);
                 try {
-                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-10 0 ~-10 ~10 0 ~10 bedrock`, World.getDimension("nether"));
+                    player.runCommand(`fill ~-10 0 ~-10 ~10 0 ~10 bedrock`);
                 } catch (error) {}
 
                 try {
-                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-10 127 ~-10 ~10 127 ~10 bedrock`, World.getDimension("nether"));
+                    player.runCommand(`fill ~-10 127 ~-10 ~10 127 ~10 bedrock`);
                 } catch (error) {}
 
                 try {
-                    Commands.run(`execute @a[name="${player.nameTag}",rm=0,scores={bedrock=1..}] ~~~ fill ~-5 5 ~-5 ~5 120 ~5 air 0 replace bedrock`, World.getDimension("nether"));
+                    player.runCommand(`fill ~-5 5 ~-5 ~5 120 ~5 air 0 replace bedrock`);
                 } catch (error) {}
             } catch(error) {}
         }
@@ -183,10 +171,10 @@ World.events.tick.subscribe(() => {
         // reach/a
         if (config.modules.reachA.enabled && playerTags.includes('attack')) {
             try {                                                                   // we could use r=4 but that wont account for lag
-                Commands.run(`execute @a[name="${player.nameTag}",tag=attack,m=!c] ~~~ testfor @p[name=!"${player.nameTag}",r=${config.modules.reachA.reach}]`, World.getDimension("overworld"));
+                player.runCommand(`execute @s[tag=attack,m=!c] ~~~ testfor @p[name=!"${player.nameTag}",r=${config.modules.reachA.reach}]`);
             } catch (error) {
                 try {
-                    Commands.run(`execute @a[name="${player.nameTag}",tag=attack,m=!c] ~~~ function checks/alerts/reach`, World.getDimension("overworld"));
+                    player.runCommand(`execute @s[tag=attack,m=!c] ~~~ function checks/alerts/reach`);
                 } catch (error2) {}
             }
         }
@@ -195,7 +183,7 @@ World.events.tick.subscribe(() => {
         if(config.modules.noslowA.enabled && Math.sqrt(Math.abs(player.velocity.x**2 + player.velocity.z**2)).toFixed(2) >= config.modules.noslowA.speed) {
             if (!player.getEffect(Minecraft.MinecraftEffectTypes.speed) && playerTags.includes('right') && playerTags.includes('ground') && !playerTags.includes('jump') && !playerTags.includes('gliding') && !playerTags.includes('swimming')) {
                 try {
-                    Commands.run(`testfor @a[name="${player.nameTag}",tag=right,tag=ground,tag=!jump,tag=!gliding,tag=!swimming]`, World.getDimension("overworld"));
+                    player.runCommand(`testfor @s[tag=right,tag=ground,tag=!jump,tag=!gliding,tag=!swimming]`);
                     flag(player, "NoSlow", "A", "Movement", "speed", Math.sqrt(Math.abs(player.velocity.x **2 + player.velocity.z **2)).toFixed(3), true, false);
                 } catch(error) {}
             }
@@ -218,7 +206,7 @@ World.events.tick.subscribe(() => {
         // invalidsprint/a = checks for sprinting with the blindness effect
         if (config.modules.invalidsprintA.enabled && player.getEffect(Minecraft.MinecraftEffectTypes.blindness) && playerTags.includes('sprint')) {
             try {
-                Commands.run(`testfor @a[name=${player.nameTag},tag=sprint]`, World.getDimension("overworld"));
+                player.runCommand(`testfor @s[tag=sprint]`);
                 flag(player, "InvalidSprint", "A", "Movement", false, false, true, false);
             } catch(error) {}
         }
@@ -227,7 +215,7 @@ World.events.tick.subscribe(() => {
         if(config.modules.flyA.enabled && Math.abs(player.velocity.y).toFixed(4) == 0.2250) {
             if(playerTags.includes('moving') && !playerTags.includes('ground') && !playerTags.includes('gliding') && !playerTags.includes('levitating') && !playerTags.includes('flying')) {
                 try {
-                    Commands.run(`testfor @a[name="${player.nameTag}",tag=moving,tag=!ground,tag=!gliding,tag=!levitating,m=!c,tag=!flying]`, World.getDimension("overworld"));
+                    player.runCommand(`testfor @s[tag=moving,tag=!ground,tag=!gliding,tag=!levitating,m=!c,tag=!flying]`);
                     flag(player, "Fly", "A", "Movement", "yVelocity", Math.abs(player.velocity.y).toFixed(4), true, false);
                 } catch(error) {}
             }
--- scripts/util.js
+++ scripts/util.js
@@ -2,7 +2,7 @@
 import * as Minecraft from "mojang-minecraft";
 
 const World = Minecraft.world;
-const Commands = Minecraft.Commands;
+// const Commands = Minecraft.Commands;
 
 /**
  * @name flag
@@ -25,31 +25,31 @@ export function flag(player, check, checkType, hackType, debugName, debug, shoul
 
     // make sure the vl objective exists
     try {
-        Commands.run(`scoreboard objectives add ${check.toLowerCase()}vl dummy`, World.getDimension("overworld"));
+        player.runCommand(`scoreboard objectives add ${check.toLowerCase()}vl dummy`);
     } catch(error) {}
 
     // cancel the message
     if (message) message.cancel = true;
 
-    if(shouldTP && check !== "Crasher") Commands.run(`tp "${player.nameTag}" "${player.nameTag}"`, World.getDimension("overworld"));
-        else if(shouldTP && check === "Crasher") Commands.run(`tp "${player.nameTag}" 30000000 30000000 30000000`, World.getDimension("overworld"));
+    if(shouldTP && check !== "Crasher") player.runCommand(`tp @s @s`);
+        else if(shouldTP && check === "Crasher") player.runCommand(`tp @s 30000000 30000000 30000000`);
 
-    Commands.run(`scoreboard players add "${player.nameTag}" ${check.toLowerCase()}vl 1`, World.getDimension("overworld"));
+    player.runCommand(`scoreboard players add @s ${check.toLowerCase()}vl 1`);
 
     try {
-        if(debug) Commands.run(`execute "${player.nameTag}" ~~~ tellraw @a[tag=notify] {"rawtext":[{"text":"§r§6[§aScythe§6]§r "},{"selector":"@s"},{"text":" §1has failed §7(${hackType}) §4${check}/${checkType} §7(${debugName}=${debug})§4. VL= "},{"score":{"name":"@s","objective":"${check.toLowerCase()}vl"}}]}`, World.getDimension("overworld"));
-            else Commands.run(`execute "${player.nameTag}" ~~~ tellraw @a[tag=notify] {"rawtext":[{"text":"§r§6[§aScythe§6]§r "},{"selector":"@s"},{"text":" §1has failed §7(${hackType}) §4${check}/${checkType}. VL= "},{"score":{"name":"@s","objective":"${check.toLowerCase()}vl"}}]}`, World.getDimension("overworld"));
+        if(debug) player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"§r§6[§aScythe§6]§r "},{"selector":"@s"},{"text":" §1has failed §7(${hackType}) §4${check}/${checkType} §7(${debugName}=${debug})§4. VL= "},{"score":{"name":"@s","objective":"${check.toLowerCase()}vl"}}]}`);
+            else player.runCommand(`tellraw @a[tag=notify] {"rawtext":[{"text":"§r§6[§aScythe§6]§r "},{"selector":"@s"},{"text":" §1has failed §7(${hackType}) §4${check}/${checkType}. VL= "},{"score":{"name":"@s","objective":"${check.toLowerCase()}vl"}}]}`);
     } catch(error) {}
 
     if (slot >= 0) {
         try {
-            if(slot <= 8) Commands.run(`replaceitem entity "${player.nameTag}" slot.hotbar ${slot} air 1`, World.getDimension("overworld"));
-                else Commands.run(`replaceitem entity "${player.nameTag}" slot.inventory ${slot - 9} air 1`, World.getDimension("overworld"));
+            if(slot <= 8) player.runCommand(`replaceitem entity @s slot.hotbar ${slot} air 1`);
+                else player.runCommand(`replaceitem entity @s slot.inventory ${slot - 9} air 1`);
         } catch(error) {console.warn(`${new Date()} | ` + error);}
     }
 
     try {
-        if (check === "Namespoof") Commands.run(`kick "${player.nameTag}" §r§6[§aScythe§6]§r Invalid username`, World.getDimension("overworld"));
+        if (check === "Namespoof") player.runCommand(`kick "${player.nameTag}" §r§6[§aScythe§6]§r Invalid username`);
     } catch(error) {
         // if we cant kick them with /kick then we instant despawn them
         player.triggerEvent("scythe:kick");
@@ -64,7 +64,7 @@ export function banMessage(player) {
     // validate that required params are defined
     if (!player) return console.warn(`${new Date()} | ` + "Error: ${player} isnt defined. Did you forget to pass it? (./util.js:68)");
 
-    let tags = Commands.run(`tag "${player.nameTag}" list`, World.getDimension('overworld')).statusMessage.replace(/§./g, '').match(/(?<=: ).*$/g);
+    let tags = player.runCommand(`tag @s list`).statusMessage.replace(/§./g, '').match(/(?<=: ).*$/g);
     if (tags) tags = String(tags).split(/[,]/);
 
     var reason;
@@ -76,7 +76,7 @@ export function banMessage(player) {
         if(t.startsWith(" reason:")) reason = t.slice(8);
     });
 
-    Commands.run(`kick "${player.nameTag}" §r\n§l§cYOU ARE BANNED!\n§r\n§eBanned By:§r ${by || "N/A"}\n§bReason:§r ${reason || "N/A"}`, World.getDimension("overworld"));
+    player.runCommand(`kick "${player.nameTag}" §r\n§l§cYOU ARE BANNED!\n§r\n§eBanned By:§r ${by || "N/A"}\n§bReason:§r ${reason || "N/A"}`);
 }
 
 /**
@@ -87,7 +87,7 @@ export function getTags(player) {
     // validate that required params are defined
     if (!player) return console.warn(`${new Date()} | ` + "Error: ${player} isnt defined. Did you forget to pass it? (./util.js:91)");
 
-    let tags = Commands.run(`tag "${player.nameTag}" list`, World.getDimension('overworld')).statusMessage.replace(/§./g, '').match(/(?<=: ).*$/g);
+    let tags = player.runCommand(`tag "${player.nameTag}" list`).statusMessage.replace(/§./g, '').match(/(?<=: ).*$/g);
 
     return String(tags);
 }
