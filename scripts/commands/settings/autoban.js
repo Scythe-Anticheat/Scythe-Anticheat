d326e5686a85be5cffc2d233aefa5429afa6b5cc
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2022-02-27 15:22:01 -0600

    fix

--- scripts/commands/settings/autoban.js
+++ scripts/commands/settings/autoban.js
@@ -8,7 +8,7 @@
 
     message.cancel = true;
 
-    let player = message.m.sender;
+    let player = message.sender;
     
     // make sure the user has permissions to run the command
     if(!player.hasTag("op")) 
