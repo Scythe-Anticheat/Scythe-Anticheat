a11c5c264b190b87f1207fafb2d42cad0381f2cc
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2022-05-10 19:55:17 -0600

    add icons to ui

--- scripts/features/ui.js
+++ scripts/features/ui.js
@@ -10,12 +10,15 @@ export function mainGui(player) {
     const form = new MinecraftUI.ActionFormData()
 		.title("Scythe Anticheat UI")
 		.body("Select an option below.")
-		.button("ban someone")
-		.button("configure settings");
+		.button("Ban Menu", "textures/ui/anvil_icon.png")
+		.button("configure settings", "textures/ui/gear.png")
+        .button("Exit", "textures/ui/redX1.png");
     form.show(player).then((response) => {
         if (response.selection == 0) {
             banMenu(player);
         }
+
+        if(response.selection == 2) return;
     });
 }
 
