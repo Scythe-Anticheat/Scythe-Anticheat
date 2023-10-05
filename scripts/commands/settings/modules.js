329ab1293dc4d779cedfa9f526a880fd93b7cb7f
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2023-10-04 18:22:36 -0700

    feat: !modules shows the enabled/disabled state of modules in the config

--- scripts/commands/settings/modules.js
+++ scripts/commands/settings/modules.js
@@ -1,8 +1,22 @@
 import { registerCommand } from "../handler.js";
+import config from "../../data/config.js";
+
+const enabled = "§8[§aENABLED§8]";
+const disabled = "§8[§4DISABLED§8]";
 
 registerCommand({
-    name: "modules",
-    execute: (message) => {
-        message.sender.runCommandAsync("function settings/modules");
-    }
+	name: "modules",
+	execute: (message) => {
+		let msg = "";
+
+		for(const module of Object.keys(config.modules)) {
+			msg += `§r§6[§aScythe§6]§r ${module} ${config.modules[module].enabled ? enabled : disabled}\n`;
+		}
+
+		for(const misc_module of Object.keys(config.misc_modules)) {
+			msg += `§r§6[§aScythe§6]§r ${misc_module} ${config.misc_modules[misc_module].enabled ? enabled : disabled}\n`;
+		}
+
+		message.sender.sendMessage(msg);
+	}
 });
